913d1b20e1c35613a9ce2b875d3bd427f1bd275f
Marciel <marciel_100@hotmail.com>
2017-11-10 07:26:19 -0200

    2-indexFuncionando
    layout e html do index funcionando

---
+++ src/main/webapp/WEB-INF/classes/dist/js/equiplano-admin.js
@@ -0,0 +1,82 @@
+/*!
+ * Equiplano BS Admin 
+ * 2016
+ */
+$(function() {
+    $('#side-menu').metisMenu();
+});
+
+//Loads the correct sidebar on window load,
+//collapses the sidebar on window resize.
+// Sets the min-height of #page-wrapper to window size
+$(function() {
+    $(window).bind("load resize", function() {
+        var topOffset = 50;
+        var width = (this.window.innerWidth > 0) ? this.window.innerWidth : this.screen.width;
+        if (width < 768) {
+            $('div.navbar-collapse').addClass('collapse');
+            topOffset = 100; // 2-row-menu
+        } else {
+            $('div.navbar-collapse').removeClass('collapse');
+        }
+
+        var height = ((this.window.innerHeight > 0) ? this.window.innerHeight : this.screen.height) - 1;
+        height = height - topOffset;
+        if (height < 1) height = 1;
+        if (height > topOffset) {
+            $("#page-wrapper").css("min-height", (height) + "px");
+        }
+    });
+
+    var url = window.location;
+    // var element = $('ul.nav a').filter(function() {
+    //     return this.href == url;
+    // }).addClass('active').parent().parent().addClass('in').parent();
+    var element = $('ul.nav a').filter(function() {
+        return this.href == url;
+    }).addClass('active').parent();
+
+    while (true) {
+        if (element.is('li')) {
+            element = element.parent().addClass('in').parent();
+        } else {
+            break;
+        }
+    }
+});
+
+
+// Prevent Bootstrap dialog from blocking focusin
+$(document).on('focusin', function(e) {
+  if ($(e.target).closest(".mce-window").length) {
+    e.stopImmediatePropagation();
+  }
+});
+
+
+// Lightbox
+$(document).ready(function() {
+    var $lightbox = $('#lightbox');
+    
+    $('[data-target="#lightbox"]').on('click', function(event) {
+        var $img = $(this).find('img'), 
+            src = $img.attr('src'),
+            alt = $img.attr('alt'),
+            css = {
+                'maxWidth': $(window).width() - 100,
+                'maxHeight': $(window).height() - 100
+            };
+    
+        $lightbox.find('.close').addClass('hidden');
+        $lightbox.find('img').attr('src', src);
+        $lightbox.find('img').attr('alt', alt);
+        $lightbox.find('img').css(css);
+    });
+    
+    $lightbox.on('shown.bs.modal', function (e) {
+        var $img = $lightbox.find('img');
+            
+        $lightbox.find('.modal-dialog').css({'width': $img.width()});
+        $lightbox.find('.close').removeClass('hidden');
+    });
+});
---
+++ src/main/webapp/WEB-INF/jsp/footer.jsp
@@ -0,0 +1,17 @@
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+		</div>
+    <!-- /#wrapper -->
+		    <footer class="rodape">
+				<div class="company-footer">
+					<div class="col-sm-2 col-xs-3">
+					<a href="http://www.equiplano.com.br" target="_blank">
+		            	<img class="img img-responsive img-rodape" alt="Equiplano" src="<c:url value='/css/images/equiplano_logo.png'/>" data-pin-nopin="true">
+		          	</a>
+		          </div>
+					<div class="col-sm-10 col-xs-9">
+						<h5>EQUIPLANO.COM.BR | ADMIN</h5>
+					</div>
+		   		</div>
+		    </footer>
+</body>
+</html>
